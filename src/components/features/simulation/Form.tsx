bfcd95b1a524ced59c280187c6704a7bd36a94bf
Filipe <filipesilveira.dev@gmail.com>
2026-06-22 11:03:14 -0300

    Estrutura inicial da página de simulação

---
+++ src/components/features/simulation/Form.tsx
@@ -0,0 +1,5 @@
+export function SimulationForm(){
+    return (
+        <h1>Formulário de Simulação</h1>
+    )
+}
--- src/router.tsx
+++ src/router.tsx
@@ -1,21 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
-import { Button } from "./components/shared/Button";
-import { PiggyBank } from "lucide-react";
 import { RootLayout } from "./components/layout/RootLayout";
+import { SimulationFormPage } from "./pages/SimulationFormPage";
 
 export const router = createBrowserRouter([
   {
     element: <RootLayout />,
-    
+
     children: [
       {
         path: "/",
-        element: (
-          <>
-            <h1>Formulário de Simulação</h1>
-            <Button variant="primary" icon={PiggyBank}>Clique Aqui</Button>
-          </>
-        ),
+        element: <SimulationFormPage />,
       },
       { path: "/resultado", element: <h1>Resultado da Simulação</h1> },
       { path: "/historico", element: <h1>Histórico da Simulação</h1> },
